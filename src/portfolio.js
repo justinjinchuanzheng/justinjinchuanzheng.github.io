d062e023e47c7417685028696a801b73b7a8f18d
Justin J. Zheng <justinzheng16@gmail.com>
2026-08-15 05:28:25 -0400

    Update manuscript submission journal

--- src/portfolio.js
+++ src/portfolio.js
@@ -126,7 +126,7 @@ const publicationsInfo = {
         "Exploring Chaos Theory through Numerical Simulations of a Bouncing Regular n-Sided Polygonal Die",
       authors:
         "Jonathan Li, Arnold Yuxuan Xie, Justin Jinchuan Zheng, and Bing Q. Li.",
-      journal: "European Journal of Physics",
+      journal: "American Journal of Physics",
       year: "2026"
     }
   ],
